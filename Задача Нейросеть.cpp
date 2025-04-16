c418cb76a7c36d887f603dbe135f1a1a7bdd0b8d
Nikita-programm <nikitka.orlov.0707@mail.ru>
2025-04-16 19:58:26 +0400

    Add files via upload

---
+++ Задача Нейросеть.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+int main() {
+    int n;
+    std::cout << "Введите количество элементов в массиве (n <= 20): ";
+    std::cin >> n;
+    if (n > 20) {
+        std::cout << "Количество элементов не должно превышать 20." << std::endl;
+        return 1;
+    }
+    std::vector<double> arr(n);
+    std::cout << "Введите " << n << " действительных чисел:" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+    double minElement = std::numeric_limits<double>::max();
+    double maxElement = std::numeric_limits<double>::lowest();
+    int minIndex = -1;
+    int maxIndex = -1;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+            minIndex = i;
+        }
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+            maxIndex = i;
+        }
+    }
+    if (minIndex != -1 && maxIndex != -1) {
+        std::swap(arr[minIndex], arr[maxIndex]);
+    }
+    std::cout << "Массив после замены наименьшего и наибольшего элементов:" << std::endl;
+    for (const double& num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
---
+++ Задача Человек.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int arr[20];
+	int n, temp, max, min;
+
+	cout << "n = ";
+	cin >> n;
+
+	if (n <= 20 && n > 0)
+	{
+		srand(time(NULL));
+
+		for (int i = 0;i < n;i++)
+		{
+			arr[i] = -10 + rand() % 21;
+			cout << arr[i] << " ";
+		}
+
+		cout << "\n";
+
+		max = 0;
+		min = 0;
+
+		for (int i = 1;i < n;i++)
+		{
+			if (arr[max] > arr[i])
+				max = i;
+			if (arr[min] < arr[i])
+				min = i;
+		}
+
+		temp = arr[max];
+		arr[max] = arr[min];
+		arr[min] = temp;
+
+		for (int i = 0;i < n;i++)
+		{
+			cout << arr[i] << " ";
+		}
+	}
+	else cout << "Вы ошиблись";
+}
